3b44e6a7d7600cc695b1d538194ff9741afed462
Tezslarichard <tezslarichard@gmail.com>
2025-01-11 23:01:29 +0100

    tomb bevezetese

--- main.js
+++ main.js
@@ -10,33 +10,61 @@ table.appendChild(tbody) // a tbodyt hozzáadom a tablehez
 const th_tr = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a fejléc adatait
 thead.appendChild(th_tr)// th_tr elemet hozzá adom a theadhez
 
-const objektum = {   //objektumba eltárolom a cellák adatait
-    th_ertek : "Nemzetiség",
-    th_ertek1 : "Szerző",
-    th_ertek2 : "Mű"
-}
+const tomb = [{   //tombba eltárolom a cellák adatait
+        th_ertek : "Nemzetiség",
+        th_ertek1 : "Szerző",
+        th_ertek2 : "Mű"
+    },
+    {  
+        Nemzetiseg:"Orosz",
+        szerzo :"Gogol",
+        mu :"A köpönyeg"
+        
+    },
+    { 
+        szerzo :"Csehov",
+        mu :"A csinovnyik halála"
+    
+    },
+    { 
+        Nemzetiseg :"Cseh",
+        szerzo :"Franz Kafka",
+        mu :"Az átváltozás"
+    },
+    {    
+        Nemzetiseg :"Magyar",
+        szerzo :"Örkény István",
+        mu :"Egyperces Novellák"
+    },
+    {   
+        Nemzetiseg :"József Attila",
+        szerzo :"Klárisok"
+    },
+    {    
+        Nemzetiseg :"Svájc",
+        szerzo :"Friedrich Dürrenmatt",
+        mu :"A fizikusok"
+    }
+    
+        
+]
 
 
 const th1 = document.createElement('th')// létrehozunk egy th elemet ez a fejléc egyik cellája
-th1.innerHTML = objektum.th_ertek // megadom az objektumot és kiválasztom hogy melyik az értéke
+th1.innerHTML = tomb[0].th_ertek //megadom a tömböt és kiválasztom melyik kell
 th_tr.appendChild(th1) //hozzáfűzöm a sorhoz
 
 
 const th2 = document.createElement('th')// létrehozunk egy th elemet ez a fejléc egyik cellája
-th2.innerHTML =objektum.th_ertek1 // megadom az objektumot és kiválasztom hogy melyik az értéke
+th2.innerHTML =tomb[0].th_ertek1 //megadom a tömböt és kiválasztom melyik kell
 th_tr.appendChild(th2)//hozzáfűzöm a sorhoz
 
 
 const th3 = document.createElement('th')// létrehozunk egy th elemet ez a fejléc egyik cellája
-th3.innerHTML = objektum.th_ertek2// megadom az objektumot és kiválasztom hogy melyik az értéke
+th3.innerHTML = tomb[0].th_ertek2//megadom a tömböt és kiválasztom melyik kell
 th_tr.appendChild(th3)//hozzáfűzöm a sorhoz
 
-const tablazat = {  //objektumba eltárolom a cellák adatait
-    td_ertek1 :"Orosz",
-    td_ertek2 :"Gogol",
-    td_ertek3 :"A köpönyeg"
 
-}
 
 // Első adat sor 
 const tr1 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
@@ -44,128 +72,103 @@ tbody.appendChild(tr1) // a sort hozzáfüzöm a tbodyhoz
 
 
 const td1 = document.createElement('td') //létrehozok egy tdt
-td1.innerHTML = tablazat.td_ertek1 //megadom az objektumot és kiválasztom hogy melyik az értéke 
+td1.innerHTML = tomb[1].Nemzetiseg //megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 td1.rowSpan = 2 //összeolvasztok 2 cellát igy az orosz az 2 sorba lesz
 tr1.appendChild(td1)//hozzáfűzöm a sorhoz
 
 
 const td2 = document.createElement('td')//létrehozok egy tdt
-td2.innerHTML = tablazat.td_ertek2//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td2.innerHTML = tomb[1].szerzo//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr1.appendChild(td2)//hozzáfűzöm a sorhoz
 
 
 const td3 = document.createElement('td')//létrehozok egy tdt
-td3.innerHTML =tablazat.td_ertek3 //megadom az objektumot és kiválasztom hogy melyik az értéke 
+td3.innerHTML =tomb[1].mu //megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr1.appendChild(td3)//hozzáfűzöm a sorhoz
 
 // Második adat sor 
 const tr2 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
 tbody.appendChild(tr2)// a sort hozzáfüzöm a tbodyhoz
 
-const tablazat1 = { //objektumba eltárolom a cellák adatait
-    td_ertek4 :"Csehov",
-    td_ertek5 :"A csinovnyik halála"
-
-}
+ 
 
 
 const td4 = document.createElement('td')//létrehozok egy tdt
-td4.innerHTML = tablazat1.td_ertek4//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td4.innerHTML = tomb[2].szerzo//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr2.appendChild(td4)//hozzáfűzöm a sorhoz
 
 
 const td5 = document.createElement('td')//létrehozok egy tdt
-td5.innerHTML = tablazat1.td_ertek5//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td5.innerHTML = tomb[2].mu//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr2.appendChild(td5)//hozzáfűzöm a sorhoz
 
 // Harmadik adat sor 
 const tr3 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
 tbody.appendChild(tr3)// a sort hozzáfüzöm a tbodyhoz
 
-const tabla = { //objektumba eltárolom a cellák adatait
-    td_ertek6 :"Cseh",
-    td_ertek7 :"Franz Kafka",
-    td_ertek8 :"Az átváltozás"
-}
-
 const td6 = document.createElement('td')//létrehozok egy tdt
-td6.innerHTML = tabla.td_ertek6//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td6.innerHTML = tomb[3].Nemzetiseg//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr3.appendChild(td6)//hozzáfűzöm a sorhoz
 
 
 const td7 = document.createElement('td')//létrehozok egy tdt
-td7.innerHTML = tabla.td_ertek7//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td7.innerHTML =tomb[3].szerzo //megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr3.appendChild(td7)//hozzáfűzöm a sorhoz
 
 
 const td8 = document.createElement('td')//létrehozok egy tdt
-td8.innerHTML = tabla.td_ertek8//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td8.innerHTML = tomb[3].mu//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr3.appendChild(td8)//hozzáfűzöm a sorhoz
 
 // Negyedik adat sor 
 const tr4 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
 tbody.appendChild(tr4)// a sort hozzáfüzöm a tbodyhoz
 
-const tabla1 = {    //objektumba eltárolom a cellák adatait
-    td_ertek9 :"Magyar",
-    td_ertek10 :"Örkény István",
-    td_ertek11 :"Egyperces Novellák"
-}
-
-
 const td9 = document.createElement('td')//létrehozok egy tdt
-td9.innerHTML = tabla1.td_ertek9 //megadom az objektumot és kiválasztom hogy melyik az értéke 
+td9.innerHTML = tomb[4].Nemzetiseg //megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 td9.rowSpan = 2//összeolvasztok 2 cellát igy a magyar az 2 sorba lesz
 tr4.appendChild(td9)//hozzáfűzöm a sorhoz
 
 
 const td10 = document.createElement('td')//létrehozok egy tdt
-td10.innerHTML = tabla1.td_ertek10//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td10.innerHTML = tomb[4].szerzo//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr4.appendChild(td10)//hozzáfűzöm a sorhoz
 
 
 const td11 = document.createElement('td')//létrehozok egy tdt
-td11.innerHTML =tabla1.td_ertek11 //megadom az objektumot és kiválasztom hogy melyik az értéke 
+td11.innerHTML =tomb[4].mu //megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr4.appendChild(td11)//hozzáfűzöm a sorhoz
 
 // Ötödik adat sor 
 const tr5 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
 tbody.appendChild(tr5)// a sort hozzáfüzöm a tbodyhoz
 
-const obj = {   //objektumba eltárolom a cellák adatait
-    td_ertek12 :"József Attila",
-    td_ertek13 :"Klárisok"
-}
+
 
 const td12 = document.createElement('td')//létrehozok egy tdt
-td12.innerHTML = obj.td_ertek12//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td12.innerHTML = tomb[5].szerzo//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr5.appendChild(td12)//hozzáfűzöm a sorhoz
 
 
 const td13 = document.createElement('td')//létrehozok egy tdt
-td13.innerHTML = obj.td_ertek13//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td13.innerHTML = tomb[5].mu//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr5.appendChild(td13)//hozzáfűzöm a sorhoz
 
 // Hatodik adat sor 
 const tr6 = document.createElement('tr')// létrehozunk egy tr elemet amibe tárolni fogjuk a táblázat adatait
 tbody.appendChild(tr6)// a sort hozzáfüzöm a tbodyhoz
 
-const tabla2 = {    //objektumba eltárolom a cellák adatait
-    td_ertek14 :"Svájc",
-    td_ertek15 :"Friedrich Dürrenmatt",
-    td_ertek16 :"A fizikusok"
-}
 
 const td14 = document.createElement('td')//létrehozok egy tdt
-td14.innerHTML = tabla2.td_ertek14//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td14.innerHTML = tomb[6].Nemzetiseg//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr6.appendChild(td14)//hozzáfűzöm a sorhoz
 
 
 const td15 = document.createElement('td')//létrehozok egy tdt
-td15.innerHTML = tabla2.td_ertek15//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td15.innerHTML = tomb[6].szerzo//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr6.appendChild(td15)//hozzáfűzöm a sorhoz
 
 
 const td16 = document.createElement('td')//létrehozok egy tdt
-td16.innerHTML = tabla2.td_ertek16//megadom az objektumot és kiválasztom hogy melyik az értéke 
+td16.innerHTML = tomb[6].mu//megadom a cellának az adatát úgy hogy kiválasztom hanyadik elem és a helyes adatot beirom
 tr6.appendChild(td16)//hozzáfűzöm a sorhoz
