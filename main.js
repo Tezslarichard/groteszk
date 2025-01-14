a9bf4622fd8d40cc48d347954f7f3d0d48d9f199
Tezslarichard <tezslarichard@gmail.com>
2025-01-14 19:02:55 +0100

    form generalas fuggveny bevezetese

--- main.js
+++ main.js
@@ -93,8 +93,133 @@ function rendermenu(tomb){ //definialom a fuggvenyem
     }
 }
 
-rendermenu(tomb) //meghivom a fuggvenyem
+function generateform(){ //létrehozom a függvényt
+    
+    const form1 = document.createElement('form') //létrehozok egy formot
+    form1.id = 'form' //megadom az idt
+    document.body.appendChild(form1) //ezt hozzáfüzöm a html törzséhez
 
+    const div = document.createElement('div') //létrehozok egy divet
+    div.classList.add('field')//megadom a classját
+
+    const label1 = document.createElement('label') //létrehozok egy labelt
+    label1.htmlFor = 'szarmazas' //megadom a forját
+    label1.innerText = "Származás: " //megadom a tartalmát
+    div.appendChild(label1) //hozzáadom a divemhez az egész labelt
+    var br = document.createElement('br') //sortörést létrehozok
+    label1.appendChild(br) //hozzáadom a labelhez
+
+
+    const input = document.createElement('input') //létrehozok egy inputot
+    input.type = 'text' //megadom a típusát hogy text
+    input.id = 'szarmazas' //megadom az idját
+    input.name = ' szarmazas' // a namet is megadom
+    div.appendChild(input) //hozzáfüzöm az inputot a divemhez
+
+
+
+    const span = document.createElement('span') // létrehozok egy spant
+    span.classList.add('error')                 //adok neki egy classt
+    div.appendChild(span)                       // ezt hozzáfüzöm a divemhez
+
+
+    const div1 = document.createElement('div')//létrehozok egy divet
+    div1.classList.add('field')//megadom a classját
+
+    const label2 = document.createElement('label')//létrehozok egy labelt
+    label2.htmlFor = 'szerzo1'//megadom a forját
+    label2.innerText = "1. szerző: "//megadom a tartalmát
+    div1.appendChild(label2)//hozzáadom a divemhez az egész labelt
+    var br1 = document.createElement('br')//sortörést létrehozok
+    label2.appendChild(br1)//hozzáadom a labelhez
+
+
+    const input1 = document.createElement('input')//létrehozok egy inputot
+    input1.type = 'text'//megadom a típusát hogy text
+    input1.id = 'szerzo1'//megadom az idját
+    input1.name = 'szerzo1'// a namet is megadom
+    div1.appendChild(input1)//hozzáfüzöm az inputot a divemhez
+
+
+
+    const span1 = document.createElement('span')// létrehozok egy spant
+    span1.classList.add('error')//adok neki egy classt
+    div1.appendChild(span1)    // ezt hozzáfüzöm a divemhez
+
+    const div2 = document.createElement('div')//létrehozok egy divet
+    div2.classList.add('field')//megadom a classját
+
+
+    const label3 = document.createElement('label')//létrehozok egy labelt
+    label3.htmlFor = 'szerzo1mu'//megadom a forját
+    label3.innerText = "1. szerző műve: "//megadom a tartalmát
+    div2.appendChild(label3)//hozzáadom a divemhez az egész labelt
+    var br2 = document.createElement('br')//sortörést létrehozok
+    label3.appendChild(br2)//hozzáadom a labelhez
+
+    const input2 = document.createElement('input')//létrehozok egy inputot
+    input2.type = 'text'//megadom a típusát hogy text
+    input2.id = 'szerzo1mu'//megadom az idját
+    input2.name = ' szerzo1mu'// a namet is megadom
+    div2.appendChild(input2)//hozzáfüzöm az inputot a divemhez
+
+    const span2 = document.createElement('span')// létrehozok egy spant
+    span2.classList.add('error')//adok neki egy classt
+    div2.appendChild(span2)// ezt hozzáfüzöm a divemhez
+
+    const div3 = document.createElement('div')//létrehozok egy divet
+    div3.classList.add('field')//megadom a classját
+
+    const label4 = document.createElement('label')//létrehozok egy labelt
+    label4.htmlFor = 'szerzo2'//megadom a forját
+    label4.innerText = "2. szerző: "//megadom a tartalmát
+    div3.appendChild(label4)//hozzáadom a divemhez az egész labelt
+
+    var br3 = document.createElement('br')//sortörést létrehozok
+    label4.appendChild(br3)//hozzáadom a labelhez
+
+    const input3 = document.createElement('input')//létrehozok egy inputot
+    input3.type = 'text'//megadom a típusát hogy text
+    input3.id='szerzo2'//megadom az idját
+    input3.name = 'szerzo2'// a namet is megadom
+    div3.appendChild(input3)//hozzáfüzöm az inputot a divemhez
+
+    const div4 = document.createElement('div')//létrehozok egy divet
+    div4.classList.add('field')//megadom a classját
+
+    const label5 = document.createElement('label')//létrehozok egy labelt
+    label5.htmlFor = 'szerzo2mu'//megadom a forját
+    label5.innerText = "2. szerző műve: "//megadom a tartalmát
+    div4.appendChild(label5)//hozzáadom a divemhez az egész labelt
+
+    var br4 = document.createElement('br')//sortörést létrehozok
+    label5.appendChild(br4) //hozzáadom a labelhez
+
+    const input4 = document.createElement('input')//létrehozok egy inputot
+    input4.type = 'text'//megadom a típusát hogy text
+    input4.id='szerzo2mu'//megadom az idját
+    input4.name = 'szerzo2mu'// a namet is megadom
+    div4.appendChild(input4)//hozzáfüzöm az inputot a divemhez
+
+
+    const button = document.createElement('button') //létrehozok egy buttont
+    button.type = 'submit' //megadom a típusát hogy submit
+    button.innerHTML = "Hozzáadás"//megadom a tartalmát
+
+
+    form1.appendChild(div) // a diveket hozzáfüzöm a formomhoz igy megjelenik már minden
+    form1.appendChild(div1)// a diveket hozzáfüzöm a formomhoz igy megjelenik már minden
+    form1.appendChild(div2)// a diveket hozzáfüzöm a formomhoz igy megjelenik már minden
+    form1.appendChild(div3)// a diveket hozzáfüzöm a formomhoz igy megjelenik már minden
+    form1.appendChild(div4)// a diveket hozzáfüzöm a formomhoz igy megjelenik már minden
+    form1.appendChild(button)//a gombomat hozzáadom a formomhoz
+
+
+
+}
+
+rendermenu(tomb) //meghivom a fuggvenyem
+generateform() //meghivom a fuggvenyt
 
 const form = document.getElementById('form') // elkerem a form htmlelementet idval
 
@@ -165,3 +290,6 @@ function validateForm(inputhtmlElement, erroruezenet){ // definialjuk a validate
     return valid; // visszaterek a valid valtozoval
 }
 fejlecem(thertekek)
+
+
+
